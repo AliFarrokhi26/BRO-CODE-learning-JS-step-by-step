83912130ffa501f6d29db7ad61001d2464b2a90d
Ali Farrokhi <master.ali026@gmail.com>
2024-04-05 09:59:47 +0330

    YT part one && two

---
+++ dayone/index.js
@@ -0,0 +1,51 @@
+//! YT part one
+
+// console.log(`Hello`);
+// console.log(`I like pizza!`);
+
+// window.alert(`This is an alert`);
+// window.alert(`I like pizza!`);
+
+
+// document.getElementById("myH1").textContent = `Hello` ;
+// document.getElementById("myP").textContent = `I like pizza!`;
+
+//! YT part two
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+let firstName = "Ali";
+let favoriteFood = "pizza";
+let email = "pizza@gmail.com";
+
+
+console.log(typeof gpa);
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is ${gpa}`);
+
+console.log(`Your name is ${firstName}`);
+console.log(`Your favorite food is ${favoriteFood}`);
+console.log(`Your email is ${email}`);
+
+
+let online = true;
+let forsale = false;
+let student = true;
+
+console.log(online);
+console.log(`${firstName} is online : ${online}`);
+console.log(`Is this car for sale : ${forsale}`);
+console.log(`Enrolled : ${student}`);
+
+let fullName = "Ali Far";
+let age2 = 23;
+let isStudent2 = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = age2;
+document.getElementById("p3").textContent = `Enrolled : ${isStudent2}`;
+
+
