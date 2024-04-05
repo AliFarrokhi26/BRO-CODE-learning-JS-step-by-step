45b8b2f397beb9252b131486d3c23ebe6f07f1e1
Ali Farrokhi <master.ali026@gmail.com>
2024-04-05 10:53:47 +0330

    YT part three && four

--- dayone/index.js
+++ dayone/index.js
@@ -1,51 +1,23 @@
-//! YT part one
 
-// console.log(`Hello`);
-// console.log(`I like pizza!`);
-
-// window.alert(`This is an alert`);
-// window.alert(`I like pizza!`);
+//! YT part three (which was Arithmetic Operators)
 
 
-// document.getElementById("myH1").textContent = `Hello` ;
-// document.getElementById("myP").textContent = `I like pizza!`;
+//! YT part four (Accept user input)
+//?how to accept user input
 
-//! YT part two
+//? 1.Easy Way = window prompt
 
-let age = 25;
-let price = 10.99;
-let gpa = 2.1;
+// let userName;
 
-let firstName = "Ali";
-let favoriteFood = "pizza";
-let email = "pizza@gmail.com";
+// userName = window.prompt("what's your username?");
 
+// console.log(userName);
 
-console.log(typeof gpa);
-console.log(`You are ${age} years old`);
-console.log(`The price is ${price}`);
-console.log(`Your gpa is ${gpa}`);
+//? 2.Professional Way = HRML textbox
 
-console.log(`Your name is ${firstName}`);
-console.log(`Your favorite food is ${favoriteFood}`);
-console.log(`Your email is ${email}`);
+let userName;
 
-
-let online = true;
-let forsale = false;
-let student = true;
-
-console.log(online);
-console.log(`${firstName} is online : ${online}`);
-console.log(`Is this car for sale : ${forsale}`);
-console.log(`Enrolled : ${student}`);
-
-let fullName = "Ali Far";
-let age2 = 23;
-let isStudent2 = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = age2;
-document.getElementById("p3").textContent = `Enrolled : ${isStudent2}`;
-
-
+document.getElementById("mySubmit").onclick = function(){
+    userName = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${userName}`;
+}
