1448016ee4be409488be3ce1a478e257fecf2efe
Ali Farrokhi <master.ali026@gmail.com>
2024-04-06 11:07:40 +0330

    YT part 21( NUMBER GUESSING GAME )

--- dayone/index.js
+++ dayone/index.js
@@ -1,11 +1,36 @@
-//! YT part 11 to 14 skiped  
-//! YT part fifteen
+//! YT part 21
+//? NUMBER GUESSING GAME
 
-const email = "imLearningJS@gamil.com";
+const minNum = 50;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
 
-let userName = email.slice(0, email.indexOf("@"));
-console.log(userName);
 
-let extention = email.slice(email.indexOf("@") + 1);
-console.log(extention);
+
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running) {
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}`);
+    guess = Number(guess);
+
+    if (isNaN(guess)) {
+        window.alert("please enter a valid number")
+    } else if (guess > maxNum || guess < minNum) {
+        window.alert("please enter a valid number")
+    } else {
+        attempts++;
+        if (guess > answer) {
+            window.alert("TOO HIGH ! TRY AGAIN")
+        } else if (guess < answer) {
+            window.alert("TOO LOW! TRY AGAIN")
+        } else {
+            window.alert(`CORRECT! THE ANSWER WAS ${answer}. IT TOOK YOU ${attempts} ATEMPTS`);
+            running = false;
+
+        }
+    }
+}
 
