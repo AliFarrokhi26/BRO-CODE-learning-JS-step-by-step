64b7e8144234f1092f235d6e6f0cd10bda518b92
Ali Farrokhi <master.ali026@gmail.com>
2024-04-06 10:28:00 +0330

    YT part fifteen ( string slicing )

--- dayone/index.js
+++ dayone/index.js
@@ -1,28 +1,11 @@
-//! YT part 7 to 9 skiped  
-//! YT part TEN
+//! YT part 11 to 14 skiped  
+//! YT part fifteen
 
-const myCheckbox = document.getElementById("myCheckbox");
-const visaBtn = document.getElementById("visaBtn");
-const masterCardBtn = document.getElementById("masterCardBtn");
-const payPalBtn = document.getElementById("payPalBtn");
-const mySubmit = document.getElementById("mySubmit");
-const subResult = document.getElementById("subResult");
-const paymentResult = document.getElementById("paymentResult");
+const email = "imLearningJS@gamil.com";
 
-mySubmit.onclick = function () {
-    if (myCheckbox.checked) {
-        subResult.textContent = `You are subscribed!`;
-        if (visaBtn.checked) {
-            paymentResult.textContent = `You are paying with visa`;
-        } else if (masterCardBtn.checked) {
-            paymentResult.textContent = `You are paying with master card`;
-        } else if (payPalBtn.checked) {
-            paymentResult.textContent = `You are paying with paypal`;
-        } 
-    } else {
-        subResult.textContent = `You are NOT subscribed!`;
-    }
+let userName = email.slice(0, email.indexOf("@"));
+console.log(userName);
 
-
-}
+let extention = email.slice(email.indexOf("@") + 1);
+console.log(extention);
 
