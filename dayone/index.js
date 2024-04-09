105246e64040f67193b174f3dc423fc52bb0e71d
Ali Farrokhi <master.ali026@gmail.com>
2024-04-09 17:29:52 +0330

    YT part 68 (Rock - Paper - Scissors) FINAL VERSION

--- dayone/index.js
+++ dayone/index.js
@@ -5,6 +5,13 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("result");
+//-----
+let playerWinCount = 0;
+let computerWinCount = 0;
+const playerWins = document.getElementById("playerWins");
+const computerWins = document.getElementById("computerWins");
+
+
 
 function playGame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -32,14 +39,19 @@ function playGame(playerChoice) {
     resultDisplay.classList.remove("redText", "greenText");
 
 
-    switch(result){
+    switch (result) {
         case "YOU WIN!":
-        resultDisplay.classList.add("greenText");
-        break;
+            resultDisplay.classList.add("greenText");
+            playerWinCount++;
+            break;
         case "YOU LOSE!":
-        resultDisplay.classList.add("redText");
-        break;
-        
-        
+            resultDisplay.classList.add("redText");
+            computerWinCount++;
+            break;
     }
+
+    playerWins.textContent = `player wins : ${playerWinCount}`;
+    playerWins.classList.add("greenText");
+    computerWins.textContent = `computer wins : ${computerWinCount}`;
+    computerWins.classList.add("redText");
 }
