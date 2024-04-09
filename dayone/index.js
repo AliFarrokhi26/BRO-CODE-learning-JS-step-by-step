a83d5d040f54d3eeec343b77af4ca305bf975c51
Ali Farrokhi <master.ali026@gmail.com>
2024-04-09 16:10:43 +0330

    YT part 68 (Rock - Paper - Scissors )

--- dayone/index.js
+++ dayone/index.js
@@ -1,55 +1,45 @@
-//!  YT part 54  
-//?  DstopWatch 
+//!  YT part 68  
+//?  Rock - Paper - Scissors 
 
-const display = document.getElementById("display");
-let timer = null;
-let startTime = 0;
-let elapsedTime = 0;
-let isRunning = false;
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("result");
 
-function start() {
-    if (!isRunning) {
-        startTime = Date.now() - elapsedTime;
-        timer = setInterval(update, 10);
-        isRunning = true;
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if (playerChoice === computerChoice) {
+        result = "IT'S A TIE!";
+    } else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
     }
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
 
-}
+    resultDisplay.classList.remove("redText", "greenText");
 
-function stop() {
-    if (isRunning) {
-        clearInterval(timer);
-        elapsedTime = Date.now() - startTime;
-        isRunning = false;
 
+    switch(result){
+        case "YOU WIN!":
+        resultDisplay.classList.add("greenText");
+        break;
+        case "YOU LOSE!":
+        resultDisplay.classList.add("redText");
+        break;
+        
+        
     }
-
-}
-
-function reset() {
-    clearInterval(timer);
-    startTime = 0;
-    elapsedTime = 0;
-    isRunning = false;
-    display.textContent = `00:00:00:00`
-}
-
-function update() {
-
-    const currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-
-    let hour = Math.floor(elapsedTime / (1000 * 60 * 60));
-    let minute = Math.floor(elapsedTime / (1000 * 60) % 60);
-    let second = Math.floor(elapsedTime / 1000 % 60);
-    let millisecond = Math.floor(elapsedTime % 1000 / 10);
-
-    hour = String(hour).padStart(2, 0);
-    minute = String(minute).padStart(2, 0);
-    second = String(second).padStart(2, 0);
-    millisecond = String(millisecond).padStart(2, 0);
-
-    display.textContent = `${hour}:${minute}:${second}:${millisecond}`
-
-
 }
